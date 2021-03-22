6b2b901ec3704dbbc66dba0ccd1e2b4e003ae77d
Roberto Esqueda <roberto.raeh@gmail.com>
2021-03-22 00:12:44 -0600

    add: header

--- tailwind.js
+++ tailwind.js
@@ -2,10 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-green': '#42E2B8',
+        'dark-gray': '#0E141B',
+        'dark-start': '#0E141B',
+        'dark-end': '#1E2730',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
+  separator: '_',
 };
