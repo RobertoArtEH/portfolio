7109c4267764569ad7860146e9b9091769486d6d
Roberto Esqueda <roberto.raeh@gmail.com>
2021-04-22 01:22:20 -0500

    add: dynamic tag component

---
+++ src/components/Tag.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  span.tag.font-bold.rounded-lg.bg-light-gray.mr-2.mb-2 {{ title }}
+</template>
+
+<script>
+export default {
+  name: 'Tag',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
