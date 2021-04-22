b0c05ac35684cf7b98d04b6307b1a16571e73295
Roberto Esqueda <roberto.raeh@gmail.com>
2021-04-22 01:21:14 -0500

    add: projects and skills data files.

---
+++ src/data/Projects.js
@@ -0,0 +1,54 @@
+const Projects = [
+  {
+    id: null,
+    imgUrl: 'https://images.unsplash.com/photo-1518546305927-5a555bb7020d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1349&q=80',
+    title: 'XChange',
+    description: 'This is a description.',
+    tags: [
+      'Vue',
+      'Tailwind',
+    ],
+  },
+  {
+    id: null,
+    imgUrl: 'https://images.unsplash.com/photo-1518546305927-5a555bb7020d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1349&q=80',
+    title: 'XChange',
+    description: 'This is a description.',
+    tags: [
+      'Vue',
+      'Tailwind',
+    ],
+  },
+  {
+    id: null,
+    imgUrl: 'https://images.unsplash.com/photo-1518546305927-5a555bb7020d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1349&q=80',
+    title: 'XChange',
+    description: 'This is a description.',
+    tags: [
+      'Vue',
+      'Tailwind',
+    ],
+  },
+  {
+    id: null,
+    imgUrl: 'https://images.unsplash.com/photo-1518546305927-5a555bb7020d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1349&q=80',
+    title: 'XChange',
+    description: 'This is a description.',
+    tags: [
+      'Vue',
+      'Tailwind',
+    ],
+  },
+  {
+    id: null,
+    imgUrl: 'https://images.unsplash.com/photo-1518546305927-5a555bb7020d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1349&q=80',
+    title: 'XChange',
+    description: 'This is a description.',
+    tags: [
+      'Vue',
+      'Tailwind',
+    ],
+  },
+];
+
+export default Projects;
---
+++ src/data/Skills.js
@@ -0,0 +1,23 @@
+const mainSkills = [
+  'HTML & CSS',
+  'JavaScript',
+  'React',
+  'Vue',
+  'Node.js',
+  'PHP',
+  'Laravel',
+  'API REST',
+  'Git',
+  'Firebase',
+  'MySQL',
+  'MongoDB',
+];
+
+const secondarySkills = [
+  'Figma',
+  'NPM',
+  'Eslint',
+  'Github',
+];
+
+export { mainSkills, secondarySkills };
